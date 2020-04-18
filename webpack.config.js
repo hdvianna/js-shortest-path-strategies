c3e78b7d4aa17b7ced3686daec7e68f39c111476
hdvianna <hdvianna@gmail.com>
2020-04-18 20:00:59 -0300

    publishing in npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
     entry: './src/modules.js',
     output: {
-        filename: 'shortest-path-strategies.js',
+        library: 'JSShortestPathStrategies',
+        libraryExport: 'default',
+        libraryTarget: 'window',
+        filename: 'shortest-path-strategies.min.js',
         path: path.resolve(__dirname, 'dist'),
     },
-};
+},{
+    entry: './src/modules.js',
+    output: {
+        library: 'JSShortestPathStrategies',
+        libraryExport: 'default',
+        libraryTarget: 'commonjs2',
+        filename: 'shortest-path-strategies.commonjs2.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}];
