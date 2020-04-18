508ae6fd581d02e22ee6bbde63cdd6cd304d30d9
hdvianna <hdvianna@gmail.com>
2020-04-18 19:15:29 -0300

    publishing in npm

--- src/modules.js
+++ src/modules.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     GraphBuilder : require('./GraphBuilder'),
     GreedStrategy : require('./GreedyStrategy'),
     DijkstraStrategy : require('./DijkstraStrategy'),
